12253551b7ed7f2708b93ca5bc772175e1bb7b2f
RavalBhagvan <bhaggubaba1@gmail.com>
2025-12-08 22:50:57 +0530

    updated users dashboards screens

--- app/src/main/java/com/mycollagelocker/ui/theme/DashBoardScreens.kt
+++ app/src/main/java/com/mycollagelocker/ui/theme/DashBoardScreens.kt
@@ -5,36 +5,50 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun StudentDashboardScreen() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Student Dashboard", fontSize = 28.sp)
+        modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text("Student Dashboard", style = MaterialTheme.typography.headlineMedium)
+
         Spacer(Modifier.height(24.dp))
-        Text("Welcome student. Your features will be here.")
+
+        Button(onClick = { /* todo */ } ) { Text("View Documents") }
+        Button(onClick = { /* todo */ } ) { Text("Upload Documents") }
+        Button(onClick = { /* todo */ } ) { Text("Genrate QR") }
+        Button(onClick = { /* todo */ } ) { Text("Logout") }
     }
 }
 
 @Composable
 fun GuardDashboardScreen() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Guard Dashboard", fontSize = 28.sp)
+        modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text("Guard Dashboard", style = MaterialTheme.typography.headlineMedium)
+
         Spacer(Modifier.height(24.dp))
-        Text("Welcome guard. Your controls will be here.")
+
+        Button( onClick = { /* todo */ } ) { Text("Scan QR") }
+        Button( onClick = { /* todo */ } ) { Text("View Student") }
+        Button( onClick = { /* todo */ } ) { Text("Logout") }
+
     }
 }
 
+
+
+@Preview
+@Composable
+fun StudentDashboardScreenPreview() {
+    StudentDashboardScreen()
+}
+
+@Preview
+@Composable
+fun GuardDashboardScreenPreview() {
+    GuardDashboardScreen()
+}
